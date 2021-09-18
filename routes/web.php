709d9f16da178393f601f1dbe3a7eed01861aadc
q133 <blockstar2k@gmail.com>
2021-09-18 15:02:26 +0300

    user roles

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['role:admin']], function () {
+    //Routs for admin
+    Route::get('/test', function (){
+        return '123';
+    });
+});
